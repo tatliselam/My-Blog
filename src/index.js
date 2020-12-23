e3cc814ac2f3e2baf6e24167ce5504276f3f869f
Abduselam Tatli <a.selamtatli@Abduselams-MacBook-Pro.local>
2020-12-22 22:03:25 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,54 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const myBlogPost = {
+  title: 'My First Blog',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam turpis purus, rhoncus in quam ut, hendrerit scelerisque lacus. Vestibulum in semper ligula. ',
+  published: true
+}
+
+let classProperties;
+if(myBlogPost.published){
+  classProperties = 'green'
+} else {
+  classProperties = 'red'
+}
+function publishThePost(e){
+  if(e.target.innerText === 'Publish'){
+  e.target.innerText = 'Unpublish';
+  document.getElementById('title').style.color = 'green';
+} else {
+  e.target.innerText = 'Publish';
+  document.getElementById('title').style.color = 'red';
+}
+}
+const newspaper =(
+  <p>
+    <p>This picture is about programmer</p>
+
+  <img src="https://kinsta.com/wp-content/uploads/2019/08/jpg-vs-jpeg.jpg" width="90%" alt=""/>
+  <h1 id="title" className = {classProperties}>
+      {myBlogPost.title}
+    </h1>
+    <p>{myBlogPost.body}</p>
+    <button onClick={publishThePost}>Unpublish</button>
+    <img src="" alt=""/>
+  </p>
+
+)
+const blogPost = (
+  <div>
+    <h1 id="title" className = {classProperties}>
+      {myBlogPost.title}
+    </h1>
+    <p>{myBlogPost.body}</p>
+    <button onClick={publishThePost}>Unpublish</button>
+    <img src="" alt=""/>
+  </div>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  newspaper,
   document.getElementById('root')
 );
 
